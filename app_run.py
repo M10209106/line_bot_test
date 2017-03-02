8142a75287f09c10bf7f1214ee87cc1b5050c25e
chyang <ch_yang@wiwynn.com>
2017-03-02 11:17:01 +0800

    [TEST]

--- app_run.py
+++ app_run.py
@@ -31,7 +31,7 @@ def index():
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-    signature_test = signature
+    signature_test = 'aaa: ' + signature
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     try:
@@ -44,7 +44,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    content = type(signature_test).__name__
+    # content = type(signature_test).__name__
+    content = signature_test
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
     return 0
 

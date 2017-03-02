83ed3872b435cdf7d894311d4d2cbd6a46ce9a7c
chyang <ch_yang@wiwynn.com>
2017-03-02 11:09:46 +0800

    [TEST]

--- app_run.py
+++ app_run.py
@@ -44,7 +44,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    content = signature_test
+    content = type(event)
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content))
     return 0
 
